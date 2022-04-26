48920458b66c0c6410cb72f8d19223ae833f5298
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-26 00:16:54 -0400

    added react router and nav links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
@@ -9,18 +18,44 @@ import Signup from "./pages/Signup";
 import Footer from "./components/Footer";
 import "./App.css";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Home />
-      <Cart />
-      <Dashboard />
-      <Detail />
-      <Login />
-      <Signup />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/shoes/:id" component={Detail} />
+            <Route exact path="/cart" component={Cart} />
+            {/* <Route exact path="/success" component={Success} /> */}
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Nav = () => {
-  return <div className="container">Hi, I'm the navbar.</div>;
-};
+function Nav() {
+  return (
+    <header>
+      <h1>
+        <Link to="/">Sole Intentions</Link>
+      </h1>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Nav;
