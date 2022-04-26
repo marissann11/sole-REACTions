68f66a71a55f2d849e8b51980927872ab60a80bb
tbrooks <tbrooks321@yahoo.com>
2022-04-25 22:08:50 -0400

    added User model

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+const Order = require("./Order");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    required: "Email address is required",
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  orders: [Order.schema],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
