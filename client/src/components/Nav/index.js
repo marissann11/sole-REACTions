5141b9067d87f92a5c2355a87c68545c9b2999e4
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-28 02:45:30 -0400

    switched to responsive navbar template

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,69 +1,167 @@
-import React, { useState } from "react";
+import { createMedia } from "@artsy/fresnel";
 import { Link } from "react-router-dom";
-import { Menu, Segment, Button } from "semantic-ui-react";
-import Auth from "../../utils/auth";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import {
+  Button,
+  Container,
+  Icon,
+  Menu,
+  Segment,
+  Sidebar,
+} from "semantic-ui-react";
 
-const Nav = () => {
-  const [activeState, setActiveState] = useState({ activeItem: "home" });
-  const handleItemClick = (e, { name }) => setActiveState({ activeItem: name });
-  const { activeItem } = activeState;
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    mobile: 0,
+    tablet: 768,
+    computer: 1024,
+  },
+});
 
-  const showNav = () => {
-    if (Auth.loggedIn()) {
-      return <p>I am logged in</p>;
-    } else {
-      return (
-        <Menu inverted pointing secondary style={{ height: "6vh" }}>
-          <Menu.Item
-            as={Link}
-            to="/"
-            name="home"
-            active={activeItem === "home"}
-            onClick={handleItemClick}
-          />
-          <Menu.Item
-            as={Link}
-            to="/cart"
-            name="cart"
-            active={activeItem === "cart"}
-            onClick={handleItemClick}
-          />
-          <Menu.Item
-            as={Link}
-            to={"/dashboard"}
-            name="dash"
-            active={activeItem === "dash"}
-            onClick={handleItemClick}
-          />
-          <Menu.Item
-            position="right"
-            as={Link}
-            to={"/"}
-            name="sole intentions"
-            style={{
-              fontFamily: "Permanent Marker, cursive",
-            }}
-            active={activeItem === "sole intentions"}
-            onClick={handleItemClick}
-          />
-          <Menu.Item position="right">
-            <Button as={Link} to="/login">
-              Log In
-            </Button>
-            <Button as={Link} to="/signup" style={{ marginLeft: "0.5em" }}>
-              Sign Up
-            </Button>
-          </Menu.Item>
-        </Menu>
-      );
-    }
-  };
+class DesktopContainer extends Component {
+  state = {};
 
-  return (
-    <Segment inverted className="m-0">
-      {showNav()}
-    </Segment>
-  );
+  render() {
+    const { children } = this.props;
+    const { fixed } = this.state;
+
+    return (
+      <Media greaterThan="mobile">
+        <Segment
+          inverted
+          textAlign="center"
+          style={{ minHeight: 10, padding: "1em 0em" }}
+          vertical
+        >
+          <Menu
+            fixed={fixed ? "top" : null}
+            inverted={!fixed}
+            pointing={!fixed}
+            secondary={!fixed}
+            size="large"
+          >
+            <Container>
+              <Menu.Item as={Link} to="/">
+                Home
+              </Menu.Item>
+              <Menu.Item as={Link} to="/cart">
+                Cart
+              </Menu.Item>
+              <Menu.Item as={Link} to="/dash">
+                Dash
+              </Menu.Item>
+              <Menu.Item position="right">
+                <Button as={Link} to="/login" inverted={!fixed}>
+                  Log in
+                </Button>
+                <Button
+                  as={Link}
+                  to="/signup"
+                  inverted={!fixed}
+                  primary={fixed}
+                  style={{ marginLeft: "0.5em" }}
+                >
+                  Sign Up
+                </Button>
+              </Menu.Item>
+            </Container>
+          </Menu>
+        </Segment>
+        {children}
+      </Media>
+    );
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
 };
+
+class MobileContainer extends Component {
+  state = {};
+
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+  handleToggle = () => this.setState({ sidebarOpened: true });
+
+  render() {
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
+
+    return (
+      <Media as={Sidebar.Pushable} at="mobile">
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={sidebarOpened}
+          >
+            <Menu.Item as={Link} to="/" active>
+              Home
+            </Menu.Item>
+            <Menu.Item as={Link} to="/cart">
+              Cart
+            </Menu.Item>
+            <Menu.Item as={Link} to="/dash">
+              Dash
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Segment
+              inverted
+              textAlign="center"
+              style={{ minHeight: 10, padding: "1em 0em" }}
+              vertical
+            >
+              <Container>
+                <Menu inverted pointing secondary size="large">
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name="sidebar" />
+                  </Menu.Item>
+                  <Menu.Item position="right">
+                    <Button as={Link} to="/login" inverted>
+                      Log in
+                    </Button>
+                    <Button
+                      as={Link}
+                      to="/signup"
+                      inverted
+                      style={{ marginLeft: "0.5em" }}
+                    >
+                      Sign Up
+                    </Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+            </Segment>
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Media>
+    );
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+const ResponsiveContainer = ({ children }) => (
+  <MediaContextProvider>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </MediaContextProvider>
+);
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+const Nav = () => <ResponsiveContainer></ResponsiveContainer>;
 
 export default Nav;
