7605ea19078f56d319b57583cb7e01a3c643cf0a
wyork <w.york63@gmail.com>
2022-04-27 21:16:46 -0400

    dashboard js update

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Dashboard = () => {
-  return <div className="container">Hi, I'm the dashboard.</div>;
-};
+class Dashboard extends Component {
+  uploadWidget() {
+   window.cloudinary.openUploadWidget(
+      { cloud_name: "gw-bootcamp", upload_preset: "upload_shoes" },
+      function (error, result) {
+        console.log(result);
+      }
+    );
+  }
+  
+  render() {
+    return (
+      <body>
+        <div className="container">
+          Hi, I'm the dashboard.
+          <div className="upload">
+            <button
+              onClick={this.uploadWidget.bind(this)}
+              className="upload-button"
+            >
+              Click here to Submit Image
+            </button>
+          </div>
+        </div>
+      </body>
+    
+    );
+  }
+}
+
+
 
 export default Dashboard;
