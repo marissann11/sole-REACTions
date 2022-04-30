46831858043415063c1c2d840834891aead13da6
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-30 03:37:32 -0400

    styled sub page

--- client/src/pages/Subscription.js
+++ client/src/pages/Subscription.js
@@ -3,42 +3,88 @@ import { Button, Icon } from "semantic-ui-react";
 
 const Subscription = () => {
   return (
-    <div className="container m-5 p-5">
-      <div className="row d-flex flex-wrap justify-content-between">
-        <div className="col-6 my-5 py-5 bg-dark rounded bg-opacity-25">
-          <h3 style={{ fontFamily: "Contrail One, cursive", fontSize: "6vh" }}>
+    <div className="m-5 p-5 rounded" style={{ backgroundColor: "grey" }}>
+      <div className="row d-flex justify-content-between">
+        <div className="col-7 my-5 py-5 rounded">
+          <h3
+            style={{
+              fontFamily: "Contrail One, cursive",
+              fontSize: "6vh",
+              color: "white",
+            }}
+          >
             Join the Sneakies Community!
           </h3>
-          <p style={{ fontFamily: "Comfortaa, cursive", fontSize: "2.5vh" }}>
+          <p
+            style={{
+              fontFamily: "Comfortaa, cursive",
+              fontSize: "2.5vh",
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
             <br />
             Never miss a shoe release again! <br /> <br />
             Sole Intentions is now offering a monthly subscription that allows
             for exclusive access to all of our new releases. You'll always be
-            the first to know and never have to feel the frustration of seeing
-            'sold out' again.
+            the first to know and will never have to feel the frustration of
+            seeing 'sold out' again.
           </p>
         </div>
-        <div className="col-4 my-5 py-5 bg-dark rounded bg-opacity-25">
-          <h3 style={{ fontFamily: "Contrail One, cursive" }}>$15.00/month</h3>
-          <p style={{ fontFamily: "Comfortaa, cursive", fontSize: "2vh" }}>
+        <div className="col-5 my-5 py-5 bg-dark rounded bg-opacity-50">
+          <h3
+            style={{
+              fontFamily: "Contrail One, cursive",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            $15.00/month
+          </h3>
+          <p
+            style={{
+              fontFamily: "Comfortaa, cursive",
+              fontSize: "2vh",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            <br />
             <span>
               <Icon color="green" name="checkmark" />
             </span>
             Exclusive first-to-know access
           </p>
-          <p style={{ fontFamily: "Comfortaa, cursive", fontSize: "2vh" }}>
+          <p
+            style={{
+              fontFamily: "Comfortaa, cursive",
+              fontSize: "2vh",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
             <span>
               <Icon color="green" name="checkmark" />
             </span>
             Stay up-to-date with weekly newsletter
           </p>
-          <p style={{ fontFamily: "Comfortaa, cursive", fontSize: "2vh" }}>
+          <p
+            style={{
+              fontFamily: "Comfortaa, cursive",
+              fontSize: "2vh",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
             <span>
               <Icon color="green" name="checkmark" />
             </span>
             Free cancellation - anytime!
           </p>
-          <Button>SUBSCRIBE NOW!</Button>
+          <div style={{ textAlign: "center" }}>
+            <br />
+            <Button>SUBSCRIBE NOW!</Button>
+          </div>
         </div>
       </div>
     </div>
