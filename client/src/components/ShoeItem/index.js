510941cf9b87c28c6e4988d54de1f21cc7bf363b
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-30 21:04:34 -0400

    finished style

--- client/src/components/ShoeItem/index.js
+++ client/src/components/ShoeItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Previews/style.css";
 
 function ShoeItem(shoe) {
   const { image, name, _id, price } = shoe;
@@ -7,11 +8,13 @@ function ShoeItem(shoe) {
   return (
     <div className="ui horizontal list allCont m-5 row justify-content-start">
       <Link to={`/shoes/${_id}`} className="item prevLink" key={_id}>
-        <img
-          className="ui image shoeAll"
-          src={process.env.PUBLIC_URL + `/images/${image}`}
-          alt={name}
-        />
+        <div className="shoeDiv">
+          <img
+            className="ui image shoeAll"
+            src={process.env.PUBLIC_URL + `/images/${image}`}
+            alt={name}
+          />
+        </div>
         <div className="prevText">
           <strong>{name}</strong>
           <br />${price}
