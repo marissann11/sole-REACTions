45c3c1548f5b2b70b12bc632b837ed7376e0daa6
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-05-04 13:03:18 -0400

    styled success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-// import Jumbotron from "../components/Jumbotron";
 
 function Success() {
   useEffect(() => {
@@ -14,11 +13,18 @@ function Success() {
 
   return (
     <div>
-      {/* <Jumbotron> */}
-      <h1>Success!</h1>
-      <h2>Thank you for your purchase!</h2>
-      <h2>You will now be redirected to the home page</h2>
-      {/* </Jumbotron> */}
+      <div
+        className="m-5 p-5 text-center"
+        style={{
+          fontFamily: "Contrail One, cursive",
+          fontSize: "6vh",
+          backgroundColor: "lightgrey",
+        }}
+      >
+        <h1>Success!</h1>
+        <h2>Thank you for your purchase!</h2>
+        <h2>You will now be redirected to the home page</h2>{" "}
+      </div>
     </div>
   );
 }
