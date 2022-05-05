643bd079102f37d44942a6a24701706e1a4e0d0e
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-05-04 21:18:03 -0400

    fixed responsiveness on admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import SalesDash from '../components/SalesDash';
-import AdminManualList from '../components/AdminManualList';
-import AdminQuickSelect from '../components/AdminQuickSelect';
-import Dashboard from './Dashboard';
+import React from "react";
+import SalesDash from "../components/SalesDash";
+import AdminManualList from "../components/AdminManualList";
+import AdminQuickSelect from "../components/AdminQuickSelect";
+import Dashboard from "./Dashboard";
 
 const Admin = () => {
   return (
@@ -10,19 +10,19 @@ const Admin = () => {
       <div
         className="m-5 p-5 text-center"
         style={{
-          fontFamily: 'Contrail One, cursive',
-          fontSize: '6vh',
-          backgroundColor: 'lightgrey',
+          fontFamily: "Contrail One, cursive",
+          fontSize: "6vh",
+          backgroundColor: "lightgrey",
         }}
       >
         Administrator Dashboard
       </div>
       <div className="row justify-content-around">
         <SalesDash />
-        <div className="col-4 ms-5 mt-5">
+        <div className="col-md-4 ms-5 mt-5">
           <AdminManualList />
         </div>
-        <div className="col-7 mt-5">
+        <div className="col-md-7 mt-5">
           <Dashboard />
         </div>
         <div className="col-12 mt-5">
